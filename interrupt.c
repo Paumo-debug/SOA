94e45daa874d84aa9d293194324a961e0a464ae1
Pau <alumne@pcrecanvib5.localdomain>
2020-10-04 17:44:41 +0200

    keyboard+clock

---
+++ interrupt.c
@@ -0,0 +1,116 @@
+/*
+ * interrupt.c -
+ */
+#include <types.h>
+#include <interrupt.h>
+#include <segment.h>
+#include <hardware.h>
+#include <io.h>
+
+#include <zeos_interrupt.h>
+
+Gate idt[IDT_ENTRIES];
+Register    idtR;
+
+void keyboard_handler();
+void clock_handler();
+void sys_call_handler();
+void syscall_handler_sysenter();
+void writeMSR(int value, int index);
+
+char char_map[] =
+{
+  '\0','\0','1','2','3','4','5','6',
+  '7','8','9','0','\'','¡','\0','\0',
+  'q','w','e','r','t','y','u','i',
+  'o','p','`','+','\0','\0','a','s',
+  'd','f','g','h','j','k','l','ñ',
+  '\0','º','\0','ç','z','x','c','v',
+  'b','n','m',',','.','-','\0','*',
+  '\0','\0','\0','\0','\0','\0','\0','\0',
+  '\0','\0','\0','\0','\0','\0','\0','7',
+  '8','9','-','4','5','6','+','1',
+  '2','3','0','\0','\0','\0','<','\0',
+  '\0','\0','\0','\0','\0','\0','\0','\0',
+  '\0','\0'
+};
+
+void setInterruptHandler(int vector, void (*handler)(), int maxAccessibleFromPL)
+{
+  /***********************************************************************/
+  /* THE INTERRUPTION GATE FLAGS:                          R1: pg. 5-11  */
+  /* ***************************                                         */
+  /* flags = x xx 0x110 000 ?????                                        */
+  /*         |  |  |                                                     */
+  /*         |  |   \ D = Size of gate: 1 = 32 bits; 0 = 16 bits         */
+  /*         |   \ DPL = Num. higher PL from which it is accessible      */
+  /*          \ P = Segment Present bit                                  */
+  /***********************************************************************/
+  Word flags = (Word)(maxAccessibleFromPL << 13);
+  flags |= 0x8E00;    /* P = 1, D = 1, Type = 1110 (Interrupt Gate) */
+
+  idt[vector].lowOffset       = lowWord((DWord)handler);
+  idt[vector].segmentSelector = __KERNEL_CS;
+  idt[vector].flags           = flags;
+  idt[vector].highOffset      = highWord((DWord)handler);
+}
+
+void setTrapHandler(int vector, void (*handler)(), int maxAccessibleFromPL)
+{
+  /***********************************************************************/
+  /* THE TRAP GATE FLAGS:                                  R1: pg. 5-11  */
+  /* ********************                                                */
+  /* flags = x xx 0x111 000 ?????                                        */
+  /*         |  |  |                                                     */
+  /*         |  |   \ D = Size of gate: 1 = 32 bits; 0 = 16 bits         */
+  /*         |   \ DPL = Num. higher PL from which it is accessible      */
+  /*          \ P = Segment Present bit                                  */
+  /***********************************************************************/
+  Word flags = (Word)(maxAccessibleFromPL << 13);
+
+  //flags |= 0x8F00;    /* P = 1, D = 1, Type = 1111 (Trap Gate) */
+  /* Changed to 0x8e00 to convert it to an 'interrupt gate' and so
+     the system calls will be thread-safe. */
+  flags |= 0x8E00;    /* P = 1, D = 1, Type = 1110 (Interrupt Gate) */
+
+  idt[vector].lowOffset       = lowWord((DWord)handler);
+  idt[vector].segmentSelector = __KERNEL_CS;
+  idt[vector].flags           = flags;
+  idt[vector].highOffset      = highWord((DWord)handler);
+}
+
+//Rutina del teclat
+void keyboard_routine(){
+	//legim el contingut del registre 0x60 (keyboard)
+	Byte value = inb(0x60);	
+	if(value>>7==0){ //hem apretat el keyboard
+		char c = char_map[value&0x7F];
+		if (c=='\0') printc_xy(0,0,'C');
+		else printc_xy(0,0,c);
+	}
+}
+
+//Rutina del rellotge
+void clock_routine(){
+	zeos_show_clock();
+}
+void setIdt()
+{
+  /* Program interrups/exception service routines */
+  idtR.base  = (DWord)idt;
+  idtR.limit = IDT_ENTRIES * sizeof(Gate) - 1;
+  
+  set_handlers();
+
+  /* ADD INITIALIZATION CODE FOR INTERRUPT VECTOR */
+	setInterruptHandler(33, keyboard_handler, 0);
+	setInterruptHandler(32, clock_handler, 0);
+	setTrapHandler(0x80, sys_call_handler, 3);
+	//ho fica al enunciat, 3.9.3 - 2 (pg 37)
+	writeMSR(__KERNEL_CS, 0x174);
+	writeMSR(INITIAL_ESP, 0X175);
+	writeMSR(syscall_handler_sysenter, 0x176);
+
+  set_idt_reg(&idtR);
+}
+
---
+++ sys.c
@@ -0,0 +1,77 @@
+/*
+ * sys.c - Syscalls implementation
+ */
+#include <devices.h>
+
+#include <utils.h>
+
+#include <io.h>
+
+#include <mm.h>
+
+#include <mm_address.h>
+
+#include <sched.h>
+
+#include <errno.h>
+
+#define LECTURA 0
+#define ESCRIPTURA 1
+
+int check_fd(int fd, int permissions)
+{
+  if (fd!=1) return -9; /*EBADF*/
+  if (permissions!=ESCRIPTURA) return -13; /*EACCES*/
+  return 0;
+}
+
+int sys_ni_syscall()
+{
+	return -38; /*ENOSYS*/
+}
+
+int sys_getpid()
+{
+	return current()->PID;
+}
+
+int sys_fork()
+{
+  int PID=-1;
+
+  // creates the child process
+  
+  return PID;
+}
+
+char my_buffer[512];
+int sys_write (int fd, char * buffer, int size){
+	int result;
+	int r;
+	//comprovem si fd es correcte o no, sino es correcte fem return	
+	int fd_checked = check_fd(fd,ESCRIPTURA);	//checks if fd is correct (fd is correct when check_fd returns 0)
+	if(fd_checked != 0) return fd_checked;
+	if(buffer == NULL) return -EFAULT;		//bad address
+	if(size < 0) return -EINVAL;			//invalid argument
+
+	int blocks = size;
+	while(blocks>512){
+		copy_from_user(buffer,my_buffer,size);
+		r = sys_write_console(my_buffer, 512);
+		blocks -= r;
+		buffer += r;
+	}
+	if(blocks>0){
+		copy_from_user(buffer,my_buffer,size);
+		r = sys_write_console(my_buffer, 512);
+		blocks -= r;
+
+	}
+	result = (size - blocks);
+	return result;
+	
+}
+
+void sys_exit()
+{  
+}
